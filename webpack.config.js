00f7f1228391f7c3170f2bff1bb54aec5f00170f
Santiago Guerra <guerrasantiago19@gmail.com>
2020-05-18 17:08:26 -0500

    Configure Webpack Config

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+module.exports = {
+  mode: 'development',
+  entry: path.join(__dirname, 'game', 'index'),
+  watch: true,
+  output: {
+    path: path.join(__dirname, 'dist'),
+    publicPath: '/dist/',
+    filename: "bundle.js",
+    chunkFilename: '[name].js'
+  },
+  module: {
+    rules: [{
+      test: /.jsx?$/,
+      include: [
+        path.resolve(__dirname, 'game')
+      ],
+      exclude: [
+        path.resolve(__dirname, 'node_modules')
+      ],
+      loader: 'babel-loader',
+      query: {
+        presets: [
+          ["@babel/env", {
+            "targets": {
+              "browsers": "last 2 chrome versions"
+            }
+          }]
+        ]
+      }
+    }]
+  },
+  resolve: {
+    extensions: ['.json', '.js', '.jsx']
+  },
+  devtool: 'source-map',
+};
