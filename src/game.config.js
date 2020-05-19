a7f8b6452805d359ebaa8a7362d0c65fe1d16456
Santiago Guerra <guerrasantiago19@gmail.com>
2020-05-19 01:16:46 -0500

    Insert create and preload function into the config game

--- src/game.config.js
+++ src/game.config.js
@@ -1,11 +1,16 @@
 import Phaser from 'phaser';
+import preload from './scene/preload';
+import create from './scene/create';
 
 export const config = {
   type: Phaser.AUTO,
   width: 1334,
   height: 750,
   backgroundColor: 0x444444,
-
+  scene: {
+    preload,
+    create
+  },
   physics: {
     default: 'arcade',
   },
