b47ef1f4da289e8770a56901c2e5e4a439cd746e
Santiago Guerra <guerrasantiago19@gmail.com>
2020-05-19 01:44:15 -0500

    Load assets from the preload function

--- src/scene/preload.js
+++ src/scene/preload.js
@@ -1,1 +1,7 @@
-export default function preload() {}
+import dude from '../assets/dude.png';
+import block from '../assets/block.png';
+
+export default function preload() {
+  this.load.image('dude', dude);
+  this.load.image('block', block);
+}
