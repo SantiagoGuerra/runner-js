6891b90cec110ce255a0837ba732df4373bd260c
Santiago Guerra <guerrasantiago19@gmail.com>
2020-05-19 09:59:35 -0500

    Create create method

--- src/scene/index.js
+++ src/scene/index.js
@@ -12,4 +12,31 @@ export default class Scene extends Phaser.Scene {
     this.load.image('dude', dude);
     this.load.image('block', block);
   }
+
+  create() {
+    this.platformGroup = this.add.group({
+
+      removeCallback(platform) {
+        platform.scene.platformPool.add(platform);
+      },
+    });
+
+    this.platformPool = this.add.group({
+
+      removeCallback(platform) {
+        platform.scene.platformGroup.add(platform);
+      },
+    });
+
+    this.playerJumps = 0;
+
+    this.addPlatform(this.sys.game.config.width, this.sys.game.config.width / 2);
+
+    this.player = this.physics.add.sprite(options.playerStartPosition, this.sys.game.config.height / 2, 'dude');
+    this.player.setGravityY(options.playerGravity);
+
+    this.physics.add.collider(this.player, this.platformGroup);
+
+    this.input.on('pointerdown', this.jump, this);
+  }
 }
