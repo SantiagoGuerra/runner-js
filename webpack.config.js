300b2266eddc050759aad32cdcd2792f5f091418
Santiago Guerra <guerrasantiago19@gmail.com>
2020-05-20 18:45:08 -0500

    Added field to player name

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
       },
     },
     {
-      test: /\.(png|jpe?g|gif|wav|xml|html)$/i,
+      test: /\.(png|jpe?g|gif|wav|xml)$/i,
       use: [
         {
           loader: 'file-loader',
